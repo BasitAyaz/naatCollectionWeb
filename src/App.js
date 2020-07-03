f05462864a55b98cdd40f6b4a201d278fd1dd01a
Basit Ahmed <basitahmed1997@gmail.com>
2020-07-03 11:58:24 +0500

    naat Collection

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{
+          fontSize:'2.5em'
+        }}>
+          Naat Collection
+        </p>
+      <button className='btn'>+</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
